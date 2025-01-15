d08c1f2289fc4da67b712746eca0c1fbf97b336d
Riyaz Shaik <riyazshaik.1104@gmail.com>
2025-01-15 12:56:48 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ const gameRoutes = require('./routes/game');
 const gameSessionRoutes = require('./routes/game_session');
 const analyticsRoutes = require('./routes/analytics');
 const exportRoutes = require('./routes/export');
-
 const { getHomePage } = require('./routes/index');
 
 const app = express();
 
-// Set up view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -25,12 +23,15 @@ app.use(bodyParser.json());
 app.use('/games', gameRoutes);
 app.use('/sessions', gameSessionRoutes);
 app.use('/analytics', analyticsRoutes);
-app.use('/', exportRoutes);
-
-// Home route
+app.use('/export', exportRoutes);
 app.get('/', getHomePage);
 
-// Start the server
-app.listen(server.port, server.host, () => {
-    console.log(`Server is running at http://${server.host}:${server.port}`);
-});
+// Export the app for testing
+module.exports = app;
+
+// Start the server only if not in test mode
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(server.port, server.host, () => {
+        console.log(`Server is running at http://${server.host}:${server.port}`);
+    });
+}
